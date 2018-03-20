8b66e968253bca5462b780ebadf5f491a156a22f
Liana Tumikyan <ltumikyan@gmail.com>
2018-03-20 13:11:36 +0200

    fixed the event detected issue

--- js/mention-script.js
+++ js/mention-script.js
@@ -1,11 +1,23 @@
 'use strict';
 (function () {
+
+    function init() {
+        let elements = {
+            textArea: $('.mention-box')
+        };
+
+        if(elements.textArea){
+            let bind = bindEvents.bind(elements);
+            bind();
+        }
+    }
+
     //fucntion detected the changes using .bind method
-    function bindEvents()
-    {
-        $(this).find('.mention-box').on('keyup', function () {
+    function bindEvents() {
+        $(this.textArea).on('keyup', function () {
             console.log('onevent');
         });
     }
-    bindEvents.bind(this);
+
+    init();
 })();
