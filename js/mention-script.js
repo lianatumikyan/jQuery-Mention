d0db37751607b4ab540b6e0073b750145cc3fb3b
Liana Tumikyan <ltumikyan@gmail.com>
2018-03-20 13:41:56 +0200

    fixed conflict in event-detect

--- js/mention-script.js
+++ js/mention-script.js
@@ -1,22 +1,88 @@
 'use strict';
 (function () {
-
-    function init() {
-        let elements = {
-            textArea: $('.mention-box')
-        };
-
-        if(elements.textArea){
-            let bind = bindEvents.bind(elements);
-            bind();
-        }
-    }
-
     //fucntion detected the changes using .bind method
     function bindEvents() {
         $(this.textArea).on('keyup', function () {
             console.log('onevent');
         });
+    }
+
+    function init() {
+        let users = [
+                {
+                    name: 'Jewel',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '1'
+                },
+                {
+                    name: 'Ren',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '2'
+                },
+                {
+                    name: 'Sonny',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '3'
+                },
+                {
+                    name: 'Lillian',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '4'
+                },
+                {
+                    name: 'Alinta',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '5'
+                },
+                {
+                    name: 'Athalie',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '6'
+                },
+                {
+                    name: 'Ederne',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '7'
+                },
+                {
+                    name: 'Eduardo',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '8'
+                },
+                {
+                    name: 'Jessica',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '9'
+                },
+                {
+                    name: 'Medea',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '10'
+                },
+                {
+                    name: 'Svetlana',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '11'
+                },
+                {
+                    name: 'Erick',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '12'
+                },
+                {
+                    name: 'Laura',
+                    image: 'https://icons8.com/icon/23242/circled-user-male-skin-type-1-2',
+                    id: '13'
+                }
+            ],
+            elements = {
+            textArea: $('.mention-box')
+             };
+
+            if(elements.textArea){
+                let bind = bindEvents.bind(elements);
+                bind();
+            }
     }
 
     init();
